d0c36ff780fe9c0bb90ac12de937ac075cb1adc9
Hiroyuki Sato <hiroysato@gmail.com>
2016-02-02 22:00:12 +0900

    Add column format definition

--- lib/embulk/parser/sisimai_analyzer.rb
+++ lib/embulk/parser/sisimai_analyzer.rb
@@ -14,9 +14,37 @@ module Embulk
           "format" => config.param("format", :string, default: "json")
         }
 
-        columns = [
-          Column.new(0, "result", :json),
-        ]
+        format = task["format"]
+        columns = case format
+        when "json"
+          [ Column.new(0, "result", :json) ]
+        when "column"
+          [
+            Column.new(0, "action", :string),
+            Column.new(1, "alias", :string),
+            Column.new(2, "deliverystatus", :string),
+            Column.new(3, "destination", :string),
+            Column.new(4, "diagnosticcode", :string),
+            Column.new(5, "diagnostictype", :string),
+            Column.new(6, "feedbacktype", :string),
+            Column.new(7, "lhost", :string),
+            Column.new(8, "listid", :string),
+            Column.new(9, "messageid", :string),
+            Column.new(10, "reason", :string),
+            Column.new(11, "recipient", :string),
+            Column.new(12, "replycode", :string),
+            Column.new(13, "senderdomain", :string),
+            Column.new(14, "smtpagent", :string),
+            Column.new(15, "smtpcommand", :string),
+            Column.new(16, "softbounce", :integer),
+            Column.new(17, "subject", :string),
+            Column.new(18, "timestamp", :long),
+            Column.new(19, "timezoneoffset", :string),
+            Column.new(20, "token", :string),
+          ]
+        else
+          raise ArgumentError,"Unkown format type: #{format}"
+        end
 
         yield(task, columns)
       end
